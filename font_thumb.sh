df8dba2e44705317dd07c1cab80700bed667faac
themanyone <nospam@thenerdshow.com>
2024-06-23 17:45:37 -0800

    turn off logging

--- font_thumb.sh
+++ font_thumb.sh
@@ -2,5 +2,5 @@
 # font_thumb
 : ${1?"
 usage: $(basename $0) input_font [output_png] [NxN (image size)]"}
-echo $1 $2 $3 >> ~/font.log
+#echo $1 $2 $3 >> ~/font.log
 convert -font "$1" -size "$3" -pointsize 50 label:"Aa" "$2"
